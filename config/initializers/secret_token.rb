f7fda9aa8f40d57520831a67f80c81499a3920ee
liaozhenhua <314773862@qq.com>
2012-03-13 22:24:31 +0800

    apache2_mysql12_3_13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogliao::Application.config.secret_token = '28e6b5ee0f0b8c9d77c675d35aabffb021039302f74f91e3504f61c86202d903bcbb92e91f00abf513d9d2d10e5c11e19293a3927fe7b13a5cf063c8c581ea73'
